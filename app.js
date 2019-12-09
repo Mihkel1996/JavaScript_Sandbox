2801ac1e8117f440543996fff615b4cc621d5e1b
Mihkel1996 <mihkeljohannes.paimla@gmail.com>
2019-12-09 10:51:57 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -51,4 +51,52 @@ mixedArray.forEach(element => {
 //the length of the string element
 console.log("Banana is a ", mixedArray[1].length, " chars long");
 
-//add elements to an array
+//add an element to an array
+mixedArray.push("apple");
+// add an element to the beginnign of the array
+mixedArray.unshift(1000);
+
+//delete the last element
+mixedArray.pop();
+//delete the first element
+mixedArray.shift();
+
+//remove an element at certain position
+mixedArray.splice(1,1);
+mixedArray.forEach(element=>{
+    console.log("Updated array: ", element);
+});
+
+
+//Objects literals
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+    email: "john.doe@gmail.com",
+    hobbies: ['music', 'sports'],
+    
+    getYearOfBirth: function() {
+        return 2019 - this.age;
+    }
+}
+
+let val;
+val = person;
+val = person.firstName;
+val = person.lastName;
+val = person['lastName'];
+val = person.age;
+val = person.hobbies[1];
+val = person.getYearOfBirth();
+console.log(val);
+
+const people = [
+    {name: "John", age: 25},
+    {name: "Sarah", age: 28},
+    {name: "Tom", age: 18}
+]
+
+people.forEach(person =>{
+    console.log(person.name, " is ", person.age, " years old");
+});
